a888f454aa0f04a183e9c15c97b02567f24bece0
Jessica Wiegers <jessica.wiegers@stud.uni-regensburg.de>
2017-08-24 22:42:07 +0200

    login function

--- Scheduler/js/src/Login.js
+++ Scheduler/js/src/Login.js
@@ -9,7 +9,9 @@ Scheduler.Login = (function () {
         signInButton,
         inputEmail,
         inputPassword,
-        passwordWrongAlert;
+        passwordWrongAlert,
+        loginWindow,
+        adminWindow;
     
    
     function checkSignIn(){
@@ -18,6 +20,12 @@ Scheduler.Login = (function () {
         console.log(inputEmail);
         if(inputEmail == email & inputPassword == password){
             console.log("hier auf Admin Seite verlinken");
+            //location.href = 'admin.html';
+           // location.href = 'admin.html';
+            
+            //Scheduler.AdminStart.init();
+            loginWindow.classList.add("hidden");
+            adminWindow.classList.remove("hidden");
         } else{
             passwordWrongAlert.classList.remove("hidden");
             window.setTimeout(function() {
@@ -32,6 +40,8 @@ Scheduler.Login = (function () {
         signInButton = document.getElementById("sign_in_button");
         signInButton.addEventListener("click", checkSignIn);
         passwordWrongAlert = document.getElementById("password-wrong");
+        loginWindow = document.getElementById("login_window");
+        adminWindow = document.getElementById("admin_window");
         
     }
     
